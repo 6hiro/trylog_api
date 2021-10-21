16b905a2ec0dfb0bbc6a08c035c2ba46df5f2e7c
6hiro <hiro6github@gmail.com>
2021-10-22 05:05:38 +0900

    デプロイ用の設定

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # DEBUG = env('DEBUG')
-DEBUG_PROPAGATE_EXCEPTIOMS = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 ALLOWED_HOSTS = ['.herokuapp.com']
